ae691c5ed98d6a96edfaaced32da743923edc82a
SaruMan64 <yete.abuna@gmail.com>
2022-08-01 00:12:41 -0300

    adicionando componente dos Cards e do HeaderBase

---
+++ web/src/components/Card.tsx
@@ -0,0 +1,39 @@
+import { Bell, BellRinging } from 'phosphor-react';
+import { useState } from 'react';
+
+type cardProps = {
+  title: string;
+  icon: JSX.Element;
+  notify?: boolean;
+  children: JSX.Element;
+};
+
+export const Card = ({ title, icon, notify, children }: cardProps) => {
+  const [notification, setNotification] = useState(false);
+
+  const funcNotification = () => {
+    if (notification) {
+      console.log('Notificação Ativa');
+    } else {
+      console.log('Notificação Desativada');
+    }
+    setNotification(!notification);
+  };
+
+  return (
+    <div className="w-5/6 px-4 py-3 border rounded-lg border-icon-dark-100">
+      <div className="h-5 w-full flex">
+        {icon}
+        <h1 className="w-full text-left pl-2 text-sm flex items-center text-header-gold">{title}</h1>
+        {notify ? (
+          <button onClick={funcNotification}>
+            {notification ? <BellRinging className='text-icon-dark-100' size={20} /> : <Bell className='text-icon-dark-100' size={20} />}
+          </button>
+        ) : (
+          false
+        )}
+      </div>
+      <div className="mt-6">{children}</div>
+    </div>
+  );
+};
--- web/src/pages/profile/index.tsx
+++ web/src/pages/profile/index.tsx
@@ -1,4 +1,7 @@
+import { Bank } from 'phosphor-react';
 import React from 'react';
+import { Card } from '../../components/Card';
+import { HeaderBase } from '../../components/HeaderBase';
 import { useUser } from '../../providers/UserProvider';
 
 /**
@@ -14,5 +17,40 @@ import { useUser } from '../../providers/UserProvider';
 export const Profile = () => {
   const { user } = useUser();
 
-  return <h1 className="text-white">{user?.name}</h1>;
+  return (
+    <>
+      <div className='h-screen w-full flex flex-col'>
+        <HeaderBase>
+          <div>teste</div>
+        </HeaderBase>
+        <div className="h-2/3 w-full flex flex-col justify-around items-center">
+          <Card
+            title="Extrato de transações"
+            icon={<Bank className="text-header-gold" size={20} />}
+            notify={true}
+          >
+            <div>
+              <p>Nome:Yeté Abunã Marques Labarca</p>
+              <p>Data de nascimento: 09/06/2001</p>
+              <p>CPF: 499.649.058-66</p>
+            </div>
+          </Card>
+          <Card
+            title="Extrato de transações"
+            icon={<Bank className="text-header-gold" size={20} />}
+            notify={false}
+          >
+            <div>
+              <p>Nome:Yeté Abunã Marques Labarca</p>
+              <p>Data de nascimento: 09/06/2001</p>
+              <p>CPF: 499.649.058-66</p>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
+
+{
+}
