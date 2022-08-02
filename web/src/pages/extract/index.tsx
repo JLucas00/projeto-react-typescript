a1a6de481e2fdcfe9ca30b687c6654f991c80bba
JLucas00 <91480284+JLucas00@users.noreply.github.com>
2022-08-02 20:01:59 -0300

    request extract

--- web/src/pages/extract/index.tsx
+++ web/src/pages/extract/index.tsx
@@ -8,11 +8,12 @@
  * Author: Rey
  */
 import { Bank } from 'phosphor-react';
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '../../components/Card';
 import { ExtractCard } from '../../components/ExtractCard';
 import { HeaderBase } from '../../components/HeaderBase';
 import { HeaderContent } from '../../components/HeaderContent';
+import { getExtract } from '../../libs/api';
 
 const mockData = {
   username: 'Dhensen',
@@ -24,10 +25,24 @@ const mockData = {
 };
 
 export const Extract = () => {
+  function handleExtract() {
+    try {
+      getExtract({
+        cpf: '12345678912',
+        agency: '5507',
+        verificationAgencyDigit: '3',
+        accountNumber: '607245',
+        verificationAccountDigit: '6',
+      }).then((response) => console.log(response.data.data.transations));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="h-screen w-full flex flex-col">
       <HeaderBase>
-        <HeaderContent content_data={mockData} />
+        <HeaderContent content_data={mockData} handleData={handleExtract} />
       </HeaderBase>
       <div className="h-2/3 w-full mt-7 flex items-center justify-center">
         <Card title="Extrato de transações" icon={<Bank />} notify={true}>
